63fc1c2eec03a0a97b32ea6671f0bd17c191f2a8
Me IsMe <georgedragon2005@yahoo.com>
2023-11-09 02:10:11 +0200

    first commit

---
+++ pun.c
@@ -0,0 +1,18 @@
+/*****************************************
+ *  Name: Pun uwu                        *
+ *  Purpose: Prints a pun supposedly     *
+ *  Date:31/10/2023                      *
+ *  Author: Me, Georgie                  *
+ *****************************************/
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("To C, or not to C:");
+    printf("that is the question \n");
+    // printf("Brevity is the soul of wit.\n --Shakespeare\n");
+    return 0;
+    
+}
