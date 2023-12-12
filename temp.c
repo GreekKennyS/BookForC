088012f9deea1911565da41b03447f820b89c07c
Me IsMe <georgedragon2005@yahoo.com>
2023-12-12 22:59:58 +0200

    hm

---
+++ temp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+int i;
+printf(" Enter a number between 1 and 4: ");
+scanf("%d", &i);
+switch
+(i) {
+    default: printf("Unrecognized number");
+        break;
+case 1: printf("One");
+break;
+
+case 2: printf("Two");
+break;
+
+case 3: printf("Three");
+break;
+
+case 4: printf("Four");
+break;
+
+
+}
+}
